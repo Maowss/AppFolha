e344f22a517dd8189c646cb81409ea52df7d1999
Maowss <Maowss@gmail.com>
2019-04-17 22:04:19 -0300

    Iniciamos o APPLoginFolha, falta terminar

---
+++ app/src/main/java/br/com/etecia/appsloginfolha/FolhaActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.appsloginfolha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FolhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.folha_layout);
+    }''
+}
