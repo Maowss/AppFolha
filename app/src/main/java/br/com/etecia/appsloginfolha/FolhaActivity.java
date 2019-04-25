8e60f24cc9412ff319328a98af89e4705f088ec1
Maowss <Maowss@gmail.com>
2019-04-24 22:06:07 -0300

    Fizemos o App Login (Folha) + Aprendemos a importar uma biblioteca (Gradle Scripts) + Fizemos a tela de formulario do App Login Folha

--- app/src/main/java/br/com/etecia/appsloginfolha/FolhaActivity.java
+++ app/src/main/java/br/com/etecia/appsloginfolha/FolhaActivity.java
@@ -1,7 +1,10 @@
 package br.com.etecia.appsloginfolha;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FolhaActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class FolhaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.folha_layout);
-    }''
+    }
+
+    public void Entrar(View view){
+        Intent entrar = new Intent(FolhaActivity.this, FormularioAPPLoginActivity.class);
+        startActivity(entrar);
+
+    }
 }
